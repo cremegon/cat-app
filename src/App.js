0be3c6f0447d81181a7ab806c6ab0da6bbeef9e2
cremegon <hussainahmed.s.shaikh@gmail.com>
2024-03-24 23:50:36 +0000

    New cat app to commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import CatComponent from "../src/cat";
+import "../src/style.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Random Cat Pictures</h1>
+      <CatComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/cat.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+const CatComponent = () => {
+  const [catUrl, setCatUrl] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const fetchCat = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        "https://api.thecatapi.com/v1/images/search"
+      );
+      const data = await response.json();
+      console.log(data);
+      if (data && data[0] && data[0].url) {
+        setCatUrl(data[0].url);
+      } else {
+        console.error("No cat pictures found");
+      }
+    } catch (error) {
+      console.error("Error fetching cat pictures", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={fetchCat} disabled={loading}>
+        {loading ? "Loading..." : "Get Random Cat"}
+      </button>
+      {loading ? (
+        <div> Loading...</div>
+      ) : (
+        catUrl && <img src={catUrl} alt="Random Cat" className="cat-pic" />
+      )}
+    </div>
+  );
+};
+
+export default CatComponent;
